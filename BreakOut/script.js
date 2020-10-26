987f4d74101da74b8f94eacea0cc346e9ce905b1
Sagar Guglani <sagar@Sagar.Guglani>
2020-10-26 14:45:32 +0530

    added touch listener

--- BreakOut/script.js
+++ BreakOut/script.js
@@ -174,7 +174,7 @@ loadAssets();
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 document.addEventListener("mousemove", mouseMoveHandler, false);
-document.addEventListener("touchmove", mouseMoveHandler, false);
+document.addEventListener("touchstart", mouseMoveHandler, false);
 
 function keyDownHandler(e) {
     if (e.key == "Right" || e.key == "ArrowRight") {
